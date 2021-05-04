9a25e58f5b1cf395357848ae6e099c3a383f7a2e
Ilya Basyrov <ilya951.com@gmail.com>
2021-05-04 17:26:09 +0500

    1

---
+++ hungry-people/js/header.js
@@ -0,0 +1,32 @@
+let headerPage=document.querySelector('.header-page');
+//header background
+window.addEventListener('scroll', setBackground);
+function setBackground() {
+    if (window.matchMedia('(max-width:992px)').matches) return;
+    if (pageYOffset<=10) headerPage.style.cssText='background:none; min-height:100px';
+    else {
+        headerPage.style.cssText='background:rgba(0, 0, 0, 0.5); min-height:50px';
+    }
+}
+
+//header links
+headerPage.addEventListener('click',function(event) {
+    //Отмена действия по умолчанию
+    event.preventDefault();
+    //Настраиваем сработку только при клике на тэг 'a'
+    let link=event.target.closest('a');
+    if (!link) return;
+    if (!headerPage.contains(link)) return;
+    //Вычисляем секцию куда будет происходить скролл
+    let target=document.querySelector('.'+link.dataset.type);
+    scrollWindow(target);   
+})
+
+
+//hamburger
+let btnMenu=document.querySelector('.btn-menu');
+btnMenu.addEventListener('click', function(event) {
+    let menu=document.querySelector('.hamburger__menu');
+    menu.classList.toggle('hidden');
+    btnMenu.classList.toggle('opened');
+});
---
+++ hungry-people/js/scroll.js
@@ -0,0 +1,26 @@
+/*scroll func*/
+let checkInterval=false;
+function scrollWindow(target) {
+    if (checkInterval)return;
+        let scrollFunc=setInterval(() => {
+        checkInterval=true;
+        let scrollPix=target.getBoundingClientRect().top;
+        if (Math.abs(scrollPix)<20) window.scrollBy(0,scrollPix/10);
+        if (Math.abs(scrollPix)<10) window.scrollBy(0,scrollPix/5);
+        if (Math.abs(scrollPix)<5) window.scrollBy(0, scrollPix);
+        else window.scrollBy(0,scrollPix/20);
+        if(Math.floor(scrollPix)==0) {
+            checkInterval=false;
+            clearInterval(scrollFunc);
+        }
+        window.addEventListener('wheel',()=>{checkInterval=false; clearInterval(scrollFunc);})
+    }, 10);
+}
+
+//btn-scroll__about
+let btnScrollAbout=document.querySelector('.btn-scroll__about');
+let sectionAbout=document.querySelector('.section-about');
+btnScrollAbout.addEventListener('click',()=>scrollWindow(sectionAbout));
+
+//btn-scroll__top
+btnScrollTop.addEventListener('click', ()=>scrollWindow(document.body));
