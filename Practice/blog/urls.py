90072407f74b6fe0ba44f542a60b57e02e6990f3
Ilya Basyrov <ilya951.com@gmail.com>
2021-06-26 20:52:41 +0500

    First commit

---
+++ Practice/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name='blog'
+urlpatterns=[
+    path('', views.post_list, name='post_list'),
+    path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+        r'(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
+]
